b07d4e4e5cc10af08bd477f050c0c012d4d79320
Luis Urbina <urbinaismael56@gmail.com>
2023-05-20 21:19:32 -0600

    commit 1

---
+++ AnalizadorSintactico/AnalizadorLexico/src/codigo/Tokens.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigo;
+
+/**
+ *
+ * @author Charly Ponce
+ */
+public enum Tokens {
+    Linea,
+    Comillas,
+    Cout,
+    T_dato,
+    Cadena,
+    Doble_Menor,
+    If,
+    Include,
+    Else,
+    Do,
+    While,
+    For,
+    Igual,
+    Suma,
+    Resta,
+    Multiplicacion,
+    Division,
+    Op_logico,
+    Op_incremento,
+    Op_relacional,
+    Op_atribucion,
+    Op_booleano,
+    Palabra,
+    Parentesis_a,
+    Parentesis_c,
+    Libreria,
+    Llave_a,
+    Llave_c,
+    Corchete_a,
+    Corchete_c,
+    Main,
+    P_coma,
+    Identificador,
+    Numero,
+    ERROR
+}
