280436e72f4c21f4e9e2f2015fb6c5b6b8a8ff5e
Luis Urbina <urbinaismael56@gmail.com>
2023-06-15 22:36:40 -0600

    Ahora si, arreglos finales del switch

--- AnalizadorSintactico/AnalizadorLexico/src/codigo/Principal.java
+++ AnalizadorSintactico/AnalizadorLexico/src/codigo/Principal.java
@@ -17,9 +17,9 @@ import java.nio.file.Paths;
  */
 public class Principal {
     public static void main(String[] args) throws Exception {
-        String ruta1 = "C:/Users/TH_Invitado/Desktop/Java/AnalizadorSintactico/AnalizadorLexico/src/codigo/Lexer.flex";
-        String ruta2 = "C:/Users/TH_Invitado/Desktop/Java/AnalizadorSintactico/AnalizadorLexico/src/codigo/LexerCup.flex";
-        String[] rutaS = {"-parser", "Sintax", "C:/Users/TH_Invitado/Desktop/Java/AnalizadorSintactico/AnalizadorLexico/src/codigo/Sintax.cup"};
+        String ruta1 = "C:/Users/pc/Desktop/Java/AnalizadorSintactico/AnalizadorLexico/src/codigo/Lexer.flex";
+        String ruta2 = "C:/Users/pc/Desktop/Java/AnalizadorSintactico/AnalizadorLexico/src/codigo/LexerCup.flex";
+        String[] rutaS = {"-parser", "Sintax", "C:/Users/pc/Desktop/Java/AnalizadorSintactico/AnalizadorLexico/src/codigo/Sintax.cup"};
         generar(ruta1, ruta2, rutaS);
     }
     
@@ -33,21 +33,21 @@ public class Principal {
         JFlex.Main.generate(archivo);
         java_cup.Main.main(rutaS);
         
-        Path rutaSym = Paths.get("C:/Users/TH_Invitado/Desktop/Java/AnalizadorSintactico/AnalizadorLexico/src/codigo/sym.java");
+        Path rutaSym = Paths.get("C:/Users/pc/Desktop/Java/AnalizadorSintactico/AnalizadorLexico/src/codigo/sym.java");
         if (Files.exists(rutaSym)) {
             Files.delete(rutaSym);
         }
         Files.move(
-                Paths.get("C:/Users/TH_Invitado/Desktop/Java/AnalizadorSintactico/AnalizadorLexico/sym.java"), 
-                Paths.get("C:/Users/TH_Invitado/Desktop/Java/AnalizadorSintactico/AnalizadorLexico/src/codigo/sym.java")
+                Paths.get("C:/Users/pc/Desktop/Java/AnalizadorSintactico/AnalizadorLexico/sym.java"), 
+                Paths.get("C:/Users/pc/Desktop/Java/AnalizadorSintactico/AnalizadorLexico/src/codigo/sym.java")
         );
-        Path rutaSin = Paths.get("C:/Users/TH_Invitado/Desktop/Java/AnalizadorSintactico/AnalizadorLexico/src/codigo/Sintax.java");
+        Path rutaSin = Paths.get("C:/Users/pc/Desktop/Java/AnalizadorSintactico/AnalizadorLexico/src/codigo/Sintax.java");
         if (Files.exists(rutaSin)) {
             Files.delete(rutaSin);
         }
         Files.move(
-                Paths.get("C:/Users/TH_Invitado/Desktop/Java/AnalizadorSintactico/AnalizadorLexico/Sintax.java"), 
-                Paths.get("C:/Users/TH_Invitado/Desktop/Java/AnalizadorSintactico/AnalizadorLexico/src/codigo/Sintax.java")
+                Paths.get("C:/Users/pc/Desktop/Java/AnalizadorSintactico/AnalizadorLexico/Sintax.java"), 
+                Paths.get("C:/Users/pc/Desktop/Java/AnalizadorSintactico/AnalizadorLexico/src/codigo/Sintax.java")
         );
     }
 }
